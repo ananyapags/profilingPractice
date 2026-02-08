d3df5f3bca077aeac56f3da7f7ebd31ba652c27a
ananyapags <apagadala@scu.edu>
2026-02-07 22:51:02 -0800

    Updated lectures

--- CudaPractice/extraLargeVec.cu
+++ CudaPractice/extraLargeVec.cu
@@ -1,6 +1,6 @@
 // How do we accommodate insanely big vectors? Lesson plus notes :)
-
-#include "cuda_runtime.h"
+#include <iostream>
+#include <cuda_runtime.h>
 #include "device_launch_parameters.h"
 #include <stdio.h>
 #include <stdlib.h>   // malloc, free, rand
@@ -12,8 +12,7 @@
 
 __global__ void test01() {
     int warpIdVal = threadIdx.x / 32;
-    printf("Block ID: %d | Thread ID: %d | Warp ID: %d\n",
-           blockIdx.x, threadIdx.x, warpIdVal);
+    printf("Block ID: %d | Thread ID: %d | Warp ID: %d\n", blockIdx.x, threadIdx.x, warpIdVal);
 }
 
 __global__ void vecAdd(int *A, int *B, int *C, int chunkSize) {
@@ -25,9 +24,9 @@ __global__ void vecAdd(int *A, int *B, int *C, int chunkSize) {
 
 void randomInts(int* x, int size) {
     for (int i = 0; i < size; i++) {
-        x[i] = rand() % 100;
+        x[i] = rand() % 100;}
     }
-}
+
 
 int main() {
     // (optional) seed rand
@@ -48,15 +47,17 @@ int main() {
     chunk_B = (int*)malloc(chunkBytes);
     chunk_C = (int*)malloc(chunkBytes);
 
+    int numBlocks = (CHUNK_SIZE+BLOCK_SIZE-1)/BLOCK_SIZE;
+
+
     for (long long offset = 0; offset < TOTAL_SIZE; offset += CHUNK_SIZE) {
 
         // current chunk size (smaller only for last chunk)
-        int currentChunkSize =
-            (TOTAL_SIZE - offset) < CHUNK_SIZE ? (int)(TOTAL_SIZE - offset) : CHUNK_SIZE;
+        int currentChunkSize = (TOTAL_SIZE - offset) < CHUNK_SIZE ? (int)(TOTAL_SIZE - offset) : CHUNK_SIZE;
 
         printf("\nOffset %lld | currentChunkSize %d\n", offset, currentChunkSize);
 
-        // random data for this chunk
+        // random data for this chunk, intaliazate each chucnk and send htese values to the gpu and use cuda memcpy
         randomInts(chunk_A, currentChunkSize);
         randomInts(chunk_B, currentChunkSize);
 
@@ -64,12 +65,10 @@ int main() {
         cudaMemcpy(d_A, chunk_A, (size_t)currentChunkSize * sizeof(int), cudaMemcpyHostToDevice);
         cudaMemcpy(d_B, chunk_B, (size_t)currentChunkSize * sizeof(int), cudaMemcpyHostToDevice);
 
-        // compute blocks based on current chunk
-        int numBlocks = (currentChunkSize + BLOCK_SIZE - 1) / BLOCK_SIZE;
-
         // launch kernel
         vecAdd<<<numBlocks, BLOCK_SIZE>>>(d_A, d_B, d_C, currentChunkSize);
-        // copy result back to host (Device -> Host)
+
+        // copy result back to host (Device -> Host), you can send htis anywhere
         cudaMemcpy(chunk_C, d_C, (size_t)currentChunkSize * sizeof(int), cudaMemcpyDeviceToHost);
 
         for (int i = 0; i < 10; i++) {
---
+++ CudaPractice/four/runtime_apis.cu
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+//queury gpu porperties
+int main() {
+    int nDevices;
+
+    cudaGetDeviceCount(&nDevices);//runtime api fucntion gets the count of the devices on out ssytem this function provied up with toal num od gpy and  saves it ndevices
+
+    //stores the porpeties of this device into a pop type structure
+    for (int i = 0; i < nDevices; i++) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("  Device Number: %d\n", i);
+        printf("  Device name: %s\n", prop.name);
+        printf("  Memory Clock Rate (KHz): %d\n",
+               prop.memoryClockRate);
+        printf("  Memory Bus Width (bits): %d\n",prop.memoryBusWidth);
+        printf("  Peak Memory Bandwidth (GB/s): %f\n\n",
+               2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+        printf("  Total global memory: %lu\n", prop.totalGlobalMem);
+        printf("  Compute capability: %d.%d\n",
+               prop.major, prop.minor);
+        printf("  Number of SMs: %d\n",
+               prop.multiProcessorCount);
+        printf("  Max threads per block: %d\n",
+               prop.maxThreadsPerBlock);
+        printf("  Max threads dimensions: x = %d, y = %d, z = %d\n",
+               prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+        printf("  Max grid dimensions: x = %d, y = %d, z = %d\n",
+               prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+    }
+    return 0;
+}
